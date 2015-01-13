fded8f469bcf2cbcf8fb0fe6d49c0a8aa6b0612d
J David Smith <emallson@archlinux.us>
2015-01-13 11:08:53 -0500

    New post

--- resources/includes/source/unbonus-tracks.zsh
+++ resources/includes/source/unbonus-tracks.zsh
@@ -3,11 +3,12 @@
 IFS=$'\n';
 
 suffix=" (Bonus Tracks Version)";
+root="/home/emallson/Music/";
 
 for songpath in $(mpc search album "$suffix"); do
     echo $songpath;
-    tag=`metaflac --show-tag=ALBUM "/mnt/quickie/Music/$songpath"`;
-    metaflac --remove-tag=ALBUM "/mnt/quickie/Music/$songpath";
+    tag=`metaflac --show-tag=ALBUM "$root$songpath"`;
+    metaflac --remove-tag=ALBUM "$root$songpath";
     metaflac --set-tag="ALBUM=${${${(C)tag#ALBUM=}%$suffix}/The /the }" \
-        "/mnt/quickie/Music/$songpath";
+        "$root$songpath";
 done
